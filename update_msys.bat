fbd10d84bb1e05fa848bf16d3f4949ae50030bef
Tad McCorkle <tad.mccorkle@gmail.com>
2020-05-10 13:43:52 -0400

    minor updates

--- update_msys.bat
+++ update_msys.bat
@@ -6,3 +6,5 @@ for %%f in (.bashrc, .bash_profile, .inputrc, _vimrc, _gvimrc) do (
 )
 copy !USERPROFILE!\bash\* .\msys2\bash\
 del .\msys2\bash\ualias >nul 2>&1
+del .\msys2\bash\ufunc >nul 2>&1
+del .\msys2\bash\upath >nul 2>&1
