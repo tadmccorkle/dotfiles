aa95fe4b9c8dc15c12cda3d3a92d216d7ba8041a
Tad McCorkle <tad.mccorkle@gmail.com>
2023-05-31 20:51:14 -0400

    Update Neovim color theme

--- .config/nvim/after/plugin/theme.lua
+++ .config/nvim/after/plugin/theme.lua
@@ -4,11 +4,22 @@ if not status then return end
 nightfox.setup({
 	options = {
 		transparent = true,
+		dim_inactive = true,
 		styles = {
 			comments = 'italic',
 			keywords = 'bold',
 		}
 	},
+	palettes = {
+		nightfox = {
+			bg0 = "#00121A",
+		}
+	},
+	groups = {
+		nightfox = {
+			WinSeparator = { fg = "fg3" },
+		}
+	}
 })
 
 vim.cmd('colorscheme nightfox')
