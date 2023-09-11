b373cc245b1051c93a672e37c1af823c454e5f8c
McCorkle, Tad <Tad.McCorkle@gtri.gatech.edu>
2023-09-11 16:31:37 -0400

    Fix markdown keymaps

--- .config/nvim/after/plugin/markdown.lua
+++ .config/nvim/after/plugin/markdown.lua
@@ -3,8 +3,8 @@ if not md_status then return end
 
 md.setup({
 	on_attach = function(bufnr)
-		vim.keymap.set({ 'n', 'x' }, '<M-l><M-o>', '<Cmd>MDListItemBelow<CR>', { buffer = bufnr })
-		vim.keymap.set({ 'n', 'x' }, '<M-L><M-O>', '<Cmd>MDListItemAbove<CR>', { buffer = bufnr })
+		vim.keymap.set({ 'n', 'i' }, '<M-l><M-o>', '<Cmd>MDListItemBelow<CR>', { buffer = bufnr })
+		vim.keymap.set({ 'n', 'i' }, '<M-L><M-O>', '<Cmd>MDListItemAbove<CR>', { buffer = bufnr })
 		vim.keymap.set('n', '<M-l><M-c>', '<Cmd>MDTaskToggle<CR>', { buffer = bufnr })
 		vim.keymap.set('x', '<M-l><M-c>', ':MDTaskToggle<CR>', { buffer = bufnr })
 	end
