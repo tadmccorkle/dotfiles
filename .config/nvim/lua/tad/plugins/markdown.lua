83078331e554f83bdb9bc834fccce080a02c0559
Tad McCorkle <tad.mccorkle@gmail.com>
2023-12-23 01:52:59 -0500

    Update markdown keymaps

--- .config/nvim/lua/tad/plugins/markdown.lua
+++ .config/nvim/lua/tad/plugins/markdown.lua
@@ -4,11 +4,12 @@ return {
 	opts = {
 		on_attach = function(bufnr)
 			local map = vim.keymap.set
-			local opts = { buffer = bufnr }
+			local opts = { buffer = bufnr, silent = true }
 			map({ 'n', 'i' }, '<M-l><M-o>', '<Cmd>MDListItemBelow<CR>', opts)
 			map({ 'n', 'i' }, '<M-L><M-O>', '<Cmd>MDListItemAbove<CR>', opts)
 			map('n', '<M-c>', '<Cmd>MDTaskToggle<CR>', opts)
 			map('x', '<M-c>', ':MDTaskToggle<CR>', opts)
+			map('n', 'gX', '<Plug>(markdown_follow_link_default_app)', opts)
 
 			local function toggle(key)
 				return "<Esc>gv<Cmd>lua require'markdown.inline'"
