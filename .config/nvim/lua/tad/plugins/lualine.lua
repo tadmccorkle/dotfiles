7e8f7b80e8782e1411dd968e3250d683422f29d9
Tad McCorkle <tad.mccorkle@gmail.com>
2024-02-23 22:28:57 -0500

    Show path with lualine

--- .config/nvim/lua/tad/plugins/lualine.lua
+++ .config/nvim/lua/tad/plugins/lualine.lua
@@ -7,6 +7,14 @@ return {
 				component_separators = {},
 				section_separators = {},
 			},
+			sections = {
+				lualine_c = {
+					{
+						'filename',
+						path = 1,
+					}
+				}
+			},
 		},
 	},
 }
