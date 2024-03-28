912de2c8dc5e59069ed2edf0730b0debed4266bd
Tad McCorkle <tad.mccorkle@gmail.com>
2024-03-28 09:42:28 -0400

    Add wezterm split keybindings

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,13 +8,37 @@ config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
 
-if wezterm.target_triple:find("windows") ~= nil then
-	config.default_prog = { "pwsh", "-nol" }
-end
+config.keys = {
+	-- split vertical
+	{
+		key = "s",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SplitPane { direction = "Right" },
+	},
+	-- split horizontal
+	{
+		key = "h",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SplitPane { direction = "Down" },
+	},
+	-- split horizontal (small)
+	{
+		key = "h",
+		mods = "ALT|SHIFT",
+		action = wezterm.action.SplitPane {
+			direction = "Down",
+			size = { Percent = 15 },
+		},
+	},
+}
 
 config.inactive_pane_hsb = {
 	saturation = 0.8,
 	brightness = 0.8,
 }
+
+if wezterm.target_triple:find("windows") ~= nil then
+	config.default_prog = { "pwsh", "-nol" }
+end
 
 return config
